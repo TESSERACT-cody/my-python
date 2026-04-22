ab000e971e7747377e5a3ef0046827fd911bf24c
TESSERACT <kirill_terp@icloud.com>
2026-04-22 16:52:45 +0300

    Delete ЗаданиеB.py

--- ЗаданиеB.py
+++
@@ -1,8 +0,0 @@
-def task_b(number):
-    # Преобразуем число в строку и выводим каждую цифру с новой строки
-    for digit in str(number):
-        print(digit)
-
-# Пример использования:
-num = int(input("Введите натуральное число: "))
-task_b(num)
